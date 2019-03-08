24830f5da91d1d7be1b03d0abf11ab7d16d5bb9c
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2019-03-09 03:05:16 +0530

    employee-visitir-add

--- vm-backend-services/controllers/web.js
+++ vm-backend-services/controllers/web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
+const pdfEmail = require('../utils/pdfEmail');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,5 +13,8 @@ router.get('/getVisitorType', (req,res) => {
   db.getVisitorType(req,res);
 });
 
+router.post('/addVisitorEmp', (req,res) => {
+  pdfEmail.generateGatePass(req,res, userData);
+});
 
 module.exports = router;
