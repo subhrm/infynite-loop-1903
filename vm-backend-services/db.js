a7376d239ab2d328d84b99221269bc5f26030b25
anubhab07 <34179359+anubhab07@users.noreply.github.com>
2019-03-08 22:00:47 +0530

    db connection update

--- vm-backend-services/db.js
+++ vm-backend-services/db.js
@@ -20,100 +20,93 @@ var con = mysql.createConnection({
 //     })
 // })
 
-
+con.connect(function(err){
+    if(err) throw err;
+});
 exports.login = function(req, res, email, password, requestedFrom) {
-    con.connect(function(err){
-        if(err) throw err;
-        const query = `select * from vms_users where email="${email}" and password="${password}" LIMIT 0,1`
-        // console.log(query)
-        try {
-            con.query(query, function(err, result) {
-                if (err) throw err;
-                // console.log(result);
-                if(result.length > 0) { 
-                    let data = result[0];
-                    const payload = {
-                        check: true
-                    };
-                    var token = jwt.sign(payload, req.app.get('Secret'), {
-                        expiresIn: 28800 // expires in 8 hours
-                    });
-    
-                    res.send({
-                        status:req.app.get('status-code').success,
-                        message: "Authentication Successful",
-                        data: {
-                            "userId": data.id,
-                            "name": data.name,
-                            "email": data.email,
-                            "userRole": data.role_code,
-                            "token": token
-                        }
-                    });
-                } else {
-                    res.send({
-                        status:req.app.get('status-code').unauthorized,
-                        message: "Invalid Credentials",
-                        data: {}
-                    });
-                }
-            });
-        } catch (error) {
-            console.log(error)
-            res.send({
-                status: req.app.get('status-code').error,
-                message: "Sorry some error occured"
-            })
-        }
-    });
+
+    const query = `select * from vms_users where email="${email}" and password="${password}" LIMIT 0,1`
+    // console.log(query)
+    try {
+        con.query(query, function(err, result) {
+            if (err) throw err;
+            // console.log(result);
+            if(result.length > 0) { 
+                let data = result[0];
+                const payload = {
+                    check: true
+                };
+                var token = jwt.sign(payload, req.app.get('Secret'), {
+                    expiresIn: 28800 // expires in 8 hours
+                });
+
+                res.send({
+                    status:req.app.get('status-code').success,
+                    message: "Authentication Successful",
+                    data: {
+                        "userId": data.id,
+                        "name": data.name,
+                        "email": data.email,
+                        "userRole": data.role_code,
+                        "token": token
+                    }
+                });
+            } else {
+                res.send({
+                    status:req.app.get('status-code').unauthorized,
+                    message: "Invalid Credentials",
+                    data: {}
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: req.app.get('status-code').error,
+            message: "Sorry some error occured"
+        })
+    }
 }
 
 
 exports.getVisitors = function(req,res) {
-    con.connect(function(err){
-        if(err) throw err;
-        let todayDate = new Date();
-        const todayStartDate = todayDate.setHours(0,0,0,0);
-        const todayEndDate = todayDate.setHours(24,0,0,0);
-        const query = `select count(*) total,  from visitor where expected_in_time>"${todayStartDate}" and expected_out_time<"${todayEndDate}"`;
-        console.log(query);
-        try {
-            con.query(query, function (err, result) {
-                if(err) throw err;
-                console.log(result);
+    if(err) throw err;
+    const query = `SELECT COUNT(1) total, SUM(IF(status = 2, 1, 0)) inside, SUM(IF(status <> 2, 1, 0)) remaining FROM visitor WHERE DATE(expected_in_time) = CURDATE();"`;
+    console.log(query);
+    try {
+        con.query(query, function (err, result) {
+            if(err) throw err;
+            console.log(result);
 
-                res.send(result);
-            })
-        } catch (error) {
-            console.log(error)
-            res.send({
-                status: req.app.get('status-code').error,
-                message: "Sorry some error occured"
-            })
-        }
-    });
+            res.send(result);
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: req.app.get('status-code').error,
+            message: "Sorry some error occured"
+        })
+    }
 }
 
 
 exports.getVisitorType = function(req,res) {
-    con.connect(function(err){
-        if(err) throw err;
-        const query = "select * from visitor_type";
-        try {
-            con.query(query, function (err, result) {
-                if(err) throw err;
-                res.send({
-                    status:req.app.get('status-code').success,
-                    message: "Fetch Successful",
-                    data: result
-                });
-            })
-        } catch (error) {
-            console.log(error)
+    if(err) throw err;
+    const query = "select * from visitor_type";
+    try {
+        con.query(query, function (err, result) {
+            if(err) throw err;
             res.send({
-                status: req.app.get('status-code').error,
-                message: "Sorry some error occured"
-            })
-        }
-    });
+                status:req.app.get('status-code').success,
+                message: "Fetch Successful",
+                data: result
+            });
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: req.app.get('status-code').error,
+            message: "Sorry some error occured"
+        })
+    }
 }
