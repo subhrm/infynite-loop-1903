08602f366ff13ce64f0ae1c0358ced5db77699cb
Nilesh Sinha <nileshinit@gmail.com>
2019-03-09 12:38:37 +0530

    visitor addition code working

--- vm-webapp/visitorApp/src/app/security-menu/guest-access/guest-access.component.ts
+++ vm-webapp/visitorApp/src/app/security-menu/guest-access/guest-access.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
 import { EmployeeService } from '../../employee-menu/employee.service';
 import { SecurityService } from '../security.service';
 
@@ -8,18 +9,39 @@ import { SecurityService } from '../security.service';
   styleUrls: ['./guest-access.component.css']
 })
 export class GuestAccessComponent implements OnInit {
-
+  statusMessage: string;
   visitorType:any[];
+  visitorTypes = [
+    {
+      code: 'B1GUEST',
+      value: 'Can Visit Only Building 1'
+    },{
+      code: 'B2GUEST',
+      value: 'Can Visist Only Building 2'
+    },{
+      code: 'B3GUEST',
+      value: 'Can Visist Only Building 3'
+    },{
+      code: 'EMPLOYEE',
+      value: 'Employee, Can visit all buildings'
+    },{
+      code: 'GEN',
+      value: 'General Visitor.Restricted Access'
+    }
+
+  ]
   name:string;
   email:string;
   mobile:string;
   referredBy:string;
-  inTime:string;
-  outTime:string;
+  inTime: Date;
+  outTime: Date;
   photoID:string;
   selectedVisitorType:string;
+  photo: any;
+  imageSource: any;
 
-  constructor(private empService:EmployeeService,private secService:SecurityService) { }
+  constructor(private empService:EmployeeService,private secService:SecurityService, private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     let self = this;
@@ -50,6 +72,7 @@ export class GuestAccessComponent implements OnInit {
       self.empService.validateImageBase64(filePayload)
       .subscribe((response)=>{
           console.log(response);
+          self.photo = response['image_id'];
         },(httpError)=>{
           console.log(httpError.error);
           alert(httpError.error.error);
@@ -59,20 +82,27 @@ export class GuestAccessComponent implements OnInit {
   }
 
   submitVisitorDetails(){
-
+    let in_time = new Date(this.inTime).toISOString();
+    in_time = in_time.replace('Z', '');
+    in_time = in_time.replace('T', ' ');
+    let out_time = new Date(this.outTime).toISOString();
+    out_time = in_time.replace('Z', '');
+    out_time = in_time.replace('T', ' ');
     let visitorPayload = {
       "Name":this.name,
       "Email":this.email,
-      "Photo":this.photoID,
+      "Photo":this.photo,
       "Mobile":this.mobile,
       "VisitorType":this.selectedVisitorType,
       "Reffered":this.referredBy,
-      "IN":this.inTime,
+      "IN": in_time,
       "OUT":this.outTime
     };
     
     this.secService.requestGuestAccess(visitorPayload)
     .subscribe(response =>{
+      this.imageSource = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' 
+      + response['data']['Photo']);
       console.log(response);
     });
   }
